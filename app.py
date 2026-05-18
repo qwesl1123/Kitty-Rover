772a439d7645f4344ca4a1de132d239f1b261e9b
qwesl1123 <wangshuocheng2002@gmail.com>
2026-05-19 07:34:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -114,4 +114,9 @@ def handle_disconnect():
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=5000,
+        allow_unsafe_werkzeug=True,
+    )
